c2d63ebc43ab4603b7c9d034a149b1eb848bd4a0
rubcuadra <rubcuadra@gmail.com>
2018-10-30 01:00:22 -0600

    al parecer ya funciona, falta juntar gui

--- Final/gui.py
+++ Final/gui.py
@@ -1,11 +1,13 @@
 import pygame
 from onitampy.board import OnitamaBoard
+from onitampy.movements import OnitamaCards
 import os
 #DO NOT TOUCH
 BOARD_SIZE = 5     # 5x5
 SCREEN = None
 CLOCK  = None
 
+BLACK    = (  0,   0,   0)
 GRAY     = (100, 100, 100)
 NAVYBLUE = ( 60,  60, 100)
 WHITE    = (255, 255, 255)
@@ -16,6 +18,7 @@ YELLOW   = (255, 255,   0)
 ORANGE   = (255, 128,   0)
 PURPLE   = (255,   0, 255)
 CYAN     = (  0, 255, 255)
+CUSTOM   = (197, 189, 186)
 
 BLUE_MASTER_PNG  = pygame.image.load(os.path.join("assets/blue_master.png"))
 RED_MASTER_PNG   = pygame.image.load(os.path.join("assets/red_master.png"))
@@ -52,11 +55,20 @@ BOARDHEIGHT = 7    # number of rows of icons
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
 
-BGCOLOR = NAVYBLUE
-LIGHTBGCOLOR = GRAY
-BOXCOLOR = WHITE
-SELECTEDBOXCOLOR = GREEN
-HIGHLIGHTCOLOR = BLUE
+BGCOLOR          = NAVYBLUE
+BOXCOLOR         = WHITE
+SELECTEDBOXCOLOR = YELLOW
+HIGHLIGHTCOLOR   = YELLOW
+
+#Onitama Logic - seran globales ya que el cliente estara cambiandolas
+selected_cell = (None,None) #TOKEN
+selected_card = None        #CARD
+valid_movs    = set([])
+from_cell     = (None,None)
+to_cell       = (None,None) #Important for client
+board         = OnitamaBoard()
+player        = OnitamaBoard.BLUE
+turn          = OnitamaBoard.BLUE
 
 def getBoxAtPixel(x, y):
     for boxx in range(BOARDWIDTH):
@@ -68,7 +80,7 @@ def getBoxAtPixel(x, y):
     return (None, None)
 
 def main():
-    global SCREEN,CLOCK
+    global SCREEN,CLOCK,selected_cell,selected_card,valid_movs,board,player,turn
     pygame.init()
     SCREEN = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     CLOCK  = pygame.time.Clock()
@@ -81,12 +93,6 @@ def main():
     firstSelection = None
     SCREEN.fill(BGCOLOR)
 
-    #Onitama Logic - seran globales
-    selected_cell = (None,None) #TOKEN
-    selected_card = (None,None) #pos CARD
-    board         = OnitamaBoard()
-    player        = OnitamaBoard.BLUE
-    turn          = OnitamaBoard.BLUE
     #Onitama Sprites
     BLUE_MASTER_PNG.convert()
     RED_MASTER_PNG.convert()
@@ -100,7 +106,9 @@ def main():
         mouseClicked = False
         SCREEN.fill(BGCOLOR) # drawing the window
         
-        drawBoard(player,turn,board,selected_cell,selected_card)
+        drawBoard(player,turn,board,selected_cell)
+        drawCards(player,turn,board,selected_card)
+        drawMovements(player,board,selected_card,selected_cell)
 
         for event in pygame.event.get(): # event handling loop
                 if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
@@ -110,14 +118,19 @@ def main():
                 elif event.type == pygame.MOUSEBUTTONUP:
                         mousex, mousey = event.pos
                         mouseClicked = True
-        #We can interact
+        
+        #We only interact if it is our turn
         if turn == player:
             #CHECK CLICK ON CELLS
             boxx, boxy = getBoxAtPixel(mousex, mousey)
             if (boxx != None and boxy != None) and mouseClicked: # The mouse is currently over a box and recieved a click
-                    token = board[boxy][boxx] if player == board.BLUE else board[BOARD_SIZE-boxy-1][BOARD_SIZE-boxx-1] 
-                    if board.isPlayer(player,token): #CHECK IT IS OUR TOKEN
-                        selected_cell = (boxx,boxy)
+                token = board[boxy][boxx] if player == board.BLUE else board[BOARD_SIZE-boxy-1][BOARD_SIZE-boxx-1] 
+                if board.isPlayer(player,token): #CHECK IT IS OUR TOKEN
+                    selected_cell = (boxx,boxy)
+                elif (boxx,boxy) in valid_movs: #CHECK CLICK ON MOVEMENT CHOICE - DESTINATION CELL
+                    from_cell = (selected_cell[1],selected_cell[0]) if player == board.BLUE else (BOARD_SIZE-1-selected_cell[1],BOARD_SIZE-1-selected_cell[0])
+                    to_cell   = (boxy,boxx)   if player == board.BLUE else (BOARD_SIZE-1-boxy,BOARD_SIZE-1-boxx)
+                    print("WE MUST MOVE,",from_cell,selected_card,to_cell) #TODO, do the movement
             #CHECK CLICK ON CARDS
             card = getClickedCard(mousex, mousey)
             if card!=None and mouseClicked:
@@ -135,7 +148,7 @@ def leftTopCoordsOfBox(boxx, boxy):
     top = boxy * (BOXSIZE + GAPSIZE) + YMARGIN
     return (left, top)
 
-def drawBoard(player,turn,board,selected_cell,selected_card):
+def drawBoard(player,turn,board,selected_cell):
     global OUR_CARDS
     # Draws cells
     for boxx in range(BOARD_SIZE):
@@ -150,8 +163,9 @@ def drawBoard(player,turn,board,selected_cell,selected_card):
             if   token == board.BLUE_MASTER:  SCREEN.blit(BLUE_MASTER_PNG,  (left,top) )
             elif token == board.RED_MASTER:   SCREEN.blit(RED_MASTER_PNG,   (left,top) )
             elif token == board.BLUE_STUDENT: SCREEN.blit(BLUE_STUDENT_PNG, (left,top) )
-            elif token == board.RED_STUDENT:  SCREEN.blit(RED_STUDENT_PNG,  (left,top) )
-    
+            elif token == board.RED_STUDENT:  SCREEN.blit(RED_STUDENT_PNG,  (left,top) )    
+
+def drawCards(player,turn,board,selected_card):
     if player == board.BLUE:
         opponentCards = board.cards[1] 
         ourCards      = board.cards[0] 
@@ -167,17 +181,34 @@ def drawBoard(player,turn,board,selected_cell,selected_card):
     #WE
     left, top = leftTopCoordsOfBox(BOARD_SIZE, 3)
     for i,card in enumerate(ourCards): 
-        SCREEN.blit(  CARDS_IMG[card],  (GAPSIZE+left+CARD_WIDTH*i,top+(BOXSIZE/2)) )
+        x = GAPSIZE+left+CARD_WIDTH*i
+        y = top+(BOXSIZE/2)
+        if selected_card == card: pygame.draw.rect(SCREEN, HIGHLIGHTCOLOR, (x - 5, y - 5, CARD_WIDTH-15 , CARD_HEIGHT + 10), 4)
+        SCREEN.blit(  CARDS_IMG[card],  (x,y) )
         OUR_CARDS[i] = card
+    
     #STAND_BY
     left, top = leftTopCoordsOfBox(BOARD_SIZE, 2)
-    
-    if turn == player: #Our turn, after we move we will get this card
+    if turn == player: #Our turn, print normally
         SCREEN.blit(  CARDS_IMG[board.cards[2]],  (GAPSIZE+left+(CARD_WIDTH/2),top-(BOXSIZE/4)-5) )
-    else:              #Opponent, after his move he will get the card
+    else:              #Opponent, upsidedown
         opponent_card = pygame.transform.rotate(CARDS_IMG[board.cards[2]], 180) #Rotate upside down
         SCREEN.blit(  opponent_card,  (GAPSIZE+left+(CARD_WIDTH/2),top-(BOXSIZE/4)-5) )
 
+#scl => selected cell
+def drawMovements(player,board,selected_card,scl):
+    global valid_movs
+    valid_movs.clear()
+    if selected_card!=None and scl[0]!=None and scl[1]!=None:
+        for movement in OnitamaCards[selected_card]:
+            x,y = scl[0]+movement[1],scl[1]+movement[0]
+            if (x>=0 and x<BOARD_SIZE) and (y>=0 and y<BOARD_SIZE): #Movement in range
+                token = board[y][x] if player == board.BLUE else board[BOARD_SIZE-1-y][BOARD_SIZE-1-x]
+                if not board.isPlayer(player,token): #VALID MOVEMENT
+                    left, top = leftTopCoordsOfBox(x, y)
+                    pygame.draw.rect(SCREEN, SELECTEDBOXCOLOR, (left, top, BOXSIZE, BOXSIZE))
+                    valid_movs.add( (x,y) )#SAVE FOR LATER
+            
 def getBoxAtPixel(x, y):
     for boxx in range(BOARD_SIZE):
         for boxy in range(BOARD_SIZE):
